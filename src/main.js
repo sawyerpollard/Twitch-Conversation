4bfe4d1d3cd69df9ba609ed9fe5bddc1668e4fc9
Sawyer Pollard <sawyerpollard@gmail.com>
2017-08-01 14:09:20 -0400

    Made code more asynchronous. Naming changed in .gitignore. Added a start script to package.json.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ const Conversation = require('watson-developer-cloud/conversation/v1');
 const MongoClient = require('mongodb').MongoClient;
 const config = require('./config.json');
 
-const client = new irc.Client('irc.chat.twitch.tv', config.twitch.username, {
-  channels: [`${config.twitch.channel} ${config.twitch.password}`],
-  debug: false,
-  password: config.twitch.password,
-  username: config.twitch.username,
-});
-
 const conversation = new Conversation({
   username: config.conversation.username,
   password: config.conversation.password,
@@ -18,26 +11,33 @@ const conversation = new Conversation({
   version_date: Conversation.VERSION_DATE_2017_04_21,
 });
 
-let from = '';
-function processResponse(err, response) {
-  if (err) {
-    console.log(err);
-    return;
+const client = new irc.Client('irc.chat.twitch.tv', config.twitch.username, {
+  channels: [`${config.twitch.channel} ${config.twitch.password}`],
+  debug: false,
+  password: config.twitch.password,
+  username: config.twitch.username,
+});
+
+function processRequest(username, message) {
+  function processResponse(err, response) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    client.say(config.twitch.channel, response.output.text[0]);
+
+    MongoClient.connect(config.mongodb.url).then(function (db) {
+      db.collection(config.mongodb.collection).update({ username },
+        { username, context: response.context }, { upsert: true });
+    }).catch(function (err) {
+      console.log('MONGODB ERROR: ', err);
+      process.exit(1);
+    });
   }
 
-  client.say(config.twitch.channel, response.output.text[0]);
-
-  MongoClient.connect('mongodb://localhost:27017/tc-db').then(function (db) {
-    db.collection('chatters').update({ username: from }, { username: from, context: response.context }, { upsert: true });
-  }).catch(function (err) {
-    console.log(err);
-  });
-}
-
-function sendToConversation(messageSender, message) {
-  MongoClient.connect('mongodb://localhost:27017/tc-db')
+  MongoClient.connect(config.mongodb.url)
     .then(function (db) {
-      db.collection('chatters').findOne({ username: messageSender })
+      db.collection(config.mongodb.collection).findOne({ username })
         .then(function (results) {
           if (results === null) {
             conversation.message({
@@ -51,21 +51,20 @@ function sendToConversation(messageSender, message) {
           }
         });
     }).catch(function (err) {
-      console.log(err);
+      console.log('MONGODB ERROR:', err);
+      process.exit(1);
     });
 }
 
-conversation.message({}, processResponse);
+client.addListener('message', (username, recipient, message) => {
+  if (username === config.twitch.username) return;
 
-client.addListener('message', (messageSender, messageRecipient, message) => {
-  from = messageSender;
   const parsedMessage = message.toLowerCase().trim();
-
   if (parsedMessage.includes(config.twitch.username)) {
-    sendToConversation(messageSender, parsedMessage.replace(config.twitch.username));
+    processRequest(username, parsedMessage.replace(config.twitch.username, ''));
   }
 });
 
 client.addListener('error', (err) => {
-  console.log(err);
+  console.log('IRC ERROR: ', err);
 });
