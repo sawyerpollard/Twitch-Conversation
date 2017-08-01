1955d78a2a79018e4bb1e1e620cfc0e98b5a5dd2
Sawyer Pollard <sawyerpollard@gmail.com>
2017-08-01 11:26:58 -0400

    Initial working version of MongoDB integration

--- src/main.js
+++ src/main.js
@@ -1,57 +1,71 @@
 const irc = require('irc');
 const Conversation = require('watson-developer-cloud/conversation/v1');
-const settings = require('./settings.json');
-const { ContextWrapper } = require('./contextwrapper');
-const { TwitchAPIGetter } = require('./twitchapigetter');
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./config.json');
 
-const contextwrapper = new ContextWrapper();
-
-const client = new irc.Client('irc.chat.twitch.tv', settings.twitch.username, {
-  channels: [`${settings.twitch.channel} ${settings.twitch.password}`],
+const client = new irc.Client('irc.chat.twitch.tv', config.twitch.username, {
+  channels: [`${config.twitch.channel} ${config.twitch.password}`],
   debug: false,
-  password: settings.twitch.password,
-  username: settings.twitch.username,
+  password: config.twitch.password,
+  username: config.twitch.username,
 });
 
 const conversation = new Conversation({
-  username: settings.conversation.username,
-  password: settings.conversation.password,
-  url: 'https://gateway.watsonplatform.net/conversation/api',
-  path: { workspace_id: settings.conversation.workspace_id },
+  username: config.conversation.username,
+  password: config.conversation.password,
+  url: config.conversation.url,
+  path: { workspace_id: config.conversation.workspace_id },
   version_date: Conversation.VERSION_DATE_2017_04_21,
 });
 
-let fromUser = null;
+let from = '';
 function processResponse(err, response) {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
-  console.log('Response: ' + response.output.text[0]);
-  if (response.output.text[0] === 'display_online_mods') {
-    client.say(settings.twitch.channel, TwitchAPIGetter.getModList());
-  } else {
-    client.say(settings.twitch.channel, response.output.text[0]);
-  }
-  contextwrapper.setContext(fromUser, response.context);
+
+  client.say(config.twitch.channel, response.output.text[0]);
+
+  MongoClient.connect('mongodb://localhost:27017/tc-db').then(function (db) {
+    db.collection('chatters').update({ username: from }, { username: from, context: response.context }, { upsert: true });
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+function sendToConversation(messageSender, message) {
+  MongoClient.connect('mongodb://localhost:27017/tc-db')
+    .then(function (db) {
+      db.collection('chatters').findOne({ username: messageSender })
+        .then(function (results) {
+          if (results === null) {
+            conversation.message({
+              input: { text: message },
+            }, processResponse);
+          } else {
+            conversation.message({
+              input: { text: message },
+              context: results.context,
+            }, processResponse);
+          }
+        });
+    }).catch(function (err) {
+      console.log(err);
+    });
 }
 
 conversation.message({}, processResponse);
 
-client.addListener('message', (from, to, message) => {
-  fromUser = from;
+client.addListener('message', (messageSender, messageRecipient, message) => {
+  from = messageSender;
   const parsedMessage = message.toLowerCase().trim();
-  if (parsedMessage.includes(settings.twitch.username)) {
-    conversation.message({
-      input: { text: parsedMessage.replace(settings.twitch.username, '') },
-      context: contextwrapper.getContext(from) },
-      processResponse);
+
+  if (parsedMessage.includes(config.twitch.username)) {
+    sendToConversation(messageSender, parsedMessage.replace(config.twitch.username));
   }
-  console.log('Parsed Message: ' + parsedMessage);
-  console.log(`Raw Message from ${from}: ${message}`);
 });
 
-client.addListener('error', (message) => {
-  console.log('Error!');
-  console.error(message);
+client.addListener('error', (err) => {
+  console.log(err);
 });
